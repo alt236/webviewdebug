8a233557449c7215b40222a4a47e2e1386290860
Alexandros Schillings <aschillings@gmail.com>
2017-08-08 21:22:54 +0100

    Improved logging

--- library/src/main/java/uk/co/alt236/webviewdebug/DebugWebViewClientLogger.java
+++ library/src/main/java/uk/co/alt236/webviewdebug/DebugWebViewClientLogger.java
@@ -2,6 +2,7 @@ package uk.co.alt236.webviewdebug;
 
 import android.annotation.TargetApi;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Message;
@@ -20,11 +21,11 @@ import android.webkit.WebView;
 
 import java.util.Locale;
 
-/*package*/ class DebugWebViewClientLogger {
+public class DebugWebViewClientLogger {
     private static final Locale LOCALE = Locale.US;
-    private static final String IN = "---> ";
-    private static final String OUT = "<--- ";
-    private static final String SPACE = "     ";
+    private static final String IN = "--->";
+    private static final String OUT = "<---";
+    private static final String SPACE = "    ";
     private static final String DEFAULT_TAG = "DEBUGCLIENT";
 
     private final LogEngine logger;
@@ -47,149 +48,175 @@ import java.util.Locale;
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void onReceivedError(final WebView view, final WebResourceRequest request, final WebResourceError error) {
         if (loggingEnabled) {
-            final int code = error.getErrorCode();
+            final Uri url = request.getUrl();
             final String method = request.getMethod();
+            final int code = error.getErrorCode();
 
-            logger.logError(String.format(LOCALE, "%sonReceivedError() 1/3 URL    : %d %s %s", SPACE, code, method, request.getUrl()));
-            logger.logError(String.format(LOCALE, "%sonReceivedError() 2/3 HEADERS: %s", SPACE, request.getRequestHeaders()));
-            logger.logError(String.format(LOCALE, "%sonReceivedError() 3/3 DESC   : %s", SPACE, error.getDescription()));
+            logger.logError(String.format(LOCALE, "%s onReceivedError() 1/3 CALL       : %d %s %s", SPACE, code, method, url));
+            logger.logError(String.format(LOCALE, "%s onReceivedError() 2/3 REQ HEADERS: %s", SPACE, request.getRequestHeaders()));
+            logger.logError(String.format(LOCALE, "%s onReceivedError() 3/3 ERR DESC   : %s", SPACE, error.getDescription()));
         }
     }
 
     public void onReceivedError(final WebView view, final int errorCode, final String description, final String failingUrl) {
         if (loggingEnabled) {
-            logger.logError(String.format(LOCALE, "%sonReceivedError() 1/2 %d %s", SPACE, errorCode, failingUrl));
-            logger.logError(String.format(LOCALE, "%sonReceivedError() 2/2 %s", SPACE, description));
+            logger.logError(String.format(LOCALE, "%s onReceivedError() 1/2 CALL: %d %s", SPACE, errorCode, failingUrl));
+            logger.logError(String.format(LOCALE, "%s onReceivedError() 2/2 ERR : %s", SPACE, description));
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void onReceivedHttpError(final WebView view, final WebResourceRequest request, final WebResourceResponse errorResponse) {
         if (loggingEnabled) {
-            logger.logError(String.format(LOCALE, "%sonReceivedHttpError() 1/2 REQ: %s", SPACE, StringUtils.toString(request)));
-            logger.logError(String.format(LOCALE, "%sonReceivedHttpError() 2/2 ERR: %s", SPACE, StringUtils.toString(errorResponse)));
+            final Uri url = request.getUrl();
+            final int code = errorResponse.getStatusCode();
+            final String method = request.getMethod();
+
+            logger.logError(String.format(LOCALE, "%s onReceivedHttpError() 1/4 CALL       : %d %s %s", SPACE, code, method, url));
+            logger.logError(String.format(LOCALE, "%s onReceivedHttpError() 2/4 REQ HEADERS: %s", SPACE, request.getRequestHeaders()));
+            logger.logError(String.format(LOCALE, "%s onReceivedHttpError() 3/4 ERR DESC   : %s", SPACE, errorResponse.getReasonPhrase()));
+            logger.logError(String.format(LOCALE, "%s onReceivedHttpError() 4/4 ERR HEADERS: %s", SPACE, errorResponse.getResponseHeaders()));
         }
     }
 
     public void onReceivedSslError(final WebView view, final SslErrorHandler handler, final SslError error) {
         if (loggingEnabled) {
-            logger.logError(String.format(LOCALE, "%sonReceivedSslError() %s", SPACE, error));
+            logger.logError(String.format(LOCALE, "%s onReceivedSslError() ERR: %s", SPACE, error));
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void shouldOverrideUrlLoading(WebView view, WebResourceRequest request, boolean retVal) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sshouldOverrideUrlLoading() %s, %s", SPACE, retVal, StringUtils.toString(request)));
+            final Uri url = request.getUrl();
+            final String method = request.getMethod();
+            final boolean redirect = request.isRedirect();
+            final boolean mainframe = request.isForMainFrame();
+            final boolean gesture = request.hasGesture();
+
+            logger.log(String.format(LOCALE, "%s shouldOverrideUrlLoading() 1/4 CALL       : %s %s", SPACE, method, url));
+            logger.log(String.format(LOCALE, "%s shouldOverrideUrlLoading() 2/4 CALL INFO  : redirect=%s, forMainFrame=%s, hasGesture=%s", SPACE, redirect, mainframe, gesture));
+            logger.log(String.format(LOCALE, "%s shouldOverrideUrlLoading() 3/4 REQ HEADERS: %s", SPACE, request.getRequestHeaders()));
+            logger.log(String.format(LOCALE, "%s shouldOverrideUrlLoading() 4/4 OVERRIDE   : %s", SPACE, retVal));
         }
     }
 
     public void shouldOverrideUrlLoading(WebView view, String url, boolean retVal) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sshouldOverrideUrlLoading() %s, %s", SPACE, retVal, url));
+            logger.log(String.format(LOCALE, "%s shouldOverrideUrlLoading() 1/2 CALL    : %s", SPACE, url));
+            logger.log(String.format(LOCALE, "%s shouldOverrideUrlLoading() 2/2 OVERRIDE: %s", SPACE, retVal));
         }
     }
 
     public void onLoadResource(WebView view, String url) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonLoadResource() %s", SPACE, url));
+            logger.log(String.format(LOCALE, "%s onLoadResource() %s", SPACE, url));
         }
     }
 
     @TargetApi(Build.VERSION_CODES.M)
     public void onPageCommitVisible(WebView view, String url) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonPageCommitVisible() %s", SPACE, url));
+            logger.log(String.format(LOCALE, "%s onPageCommitVisible() %s", SPACE, url));
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void shouldInterceptRequest(WebView view, String url, WebResourceResponse retVal) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sshouldInterceptRequest() %s %s", SPACE, url, StringUtils.toString(retVal)));
+            final String result = retVal == null ? "false" : StringUtils.toString(retVal);
+
+            logger.log(String.format(LOCALE, "%s shouldInterceptRequest() 1/2 CALL    : %s", SPACE, url));
+            logger.log(String.format(LOCALE, "%s shouldInterceptRequest() 2/2 OVERRIDE: %s", SPACE, url, result));
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void shouldInterceptRequest(WebView view, WebResourceRequest request, final WebResourceResponse retVal) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sshouldInterceptRequest() 1/2 REQ: %s", SPACE, StringUtils.toString(request)));
-            logger.log(String.format(LOCALE, "%sshouldInterceptRequest() 2/2 RES: %s", SPACE, StringUtils.toString(retVal)));
+            final Uri url = request.getUrl();
+            final String method = request.getMethod();
+            final String result = retVal == null ? "false" : StringUtils.toString(retVal);
+
+            logger.log(String.format(LOCALE, "%s shouldInterceptRequest() 1/3 CALL       : %s %s", SPACE, method, url));
+            logger.log(String.format(LOCALE, "%s shouldInterceptRequest() 2/3 REQ HEADERS: %s", SPACE, request.getRequestHeaders()));
+            logger.log(String.format(LOCALE, "%s shouldInterceptRequest() 3/3 INTERCEPT  : %s", SPACE, result));
         }
     }
 
     public void onTooManyRedirects(WebView view, Message cancelMsg, Message continueMsg) {
         if (loggingEnabled) {
-            logger.logError(String.format(LOCALE, "%sonTooManyRedirects()", SPACE));
+            logger.logError(String.format(LOCALE, "%s onTooManyRedirects()", SPACE));
         }
     }
 
     public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
         if (loggingEnabled) {
-            logger.logSecurity(String.format(LOCALE, "%sonReceivedHttpAuthRequest() %s %s %s", SPACE, host, realm, handler));
+            logger.logSecurity(String.format(LOCALE, "%s onReceivedHttpAuthRequest() %s %s %s", SPACE, host, realm, handler));
         }
     }
 
     public void onPageStarted(WebView view, String url, Bitmap facIcon) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonPageStarted()  %s", IN, url));
+            logger.log(String.format(LOCALE, "%s onPageStarted() %s", IN, url));
         }
     }
 
     public void onPageFinished(WebView view, String url) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonPageFinished() %s", OUT, url));
+            logger.log(String.format(LOCALE, "%s onPageFinished() %s", OUT, url));
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onReceivedClientCertRequest(WebView view, ClientCertRequest request) {
         if (loggingEnabled) {
-            logger.logSecurity(String.format(LOCALE, "%sonReceivedClientCertRequest() %s", SPACE, StringUtils.toString(request)));
+            logger.logSecurity(String.format(LOCALE, "%s onReceivedClientCertRequest() %s", SPACE, StringUtils.toString(request)));
         }
     }
 
     public void onFormResubmission(final WebView view, final Message dontResend, final Message resend) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonFormResubmission()", SPACE));
+            logger.log(String.format(LOCALE, "%s onFormResubmission()", SPACE));
         }
     }
 
     public void doUpdateVisitedHistory(final WebView view, final String url, final boolean isReload) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sdoUpdateVisitedHistory() %s, isReload: %s", SPACE, url, isReload));
+            logger.log(String.format(LOCALE, "%s doUpdateVisitedHistory() %s, isReload: %s", SPACE, url, isReload));
         }
     }
 
     public void shouldOverrideKeyEvent(final WebView view, final KeyEvent event, boolean retVal) {
         if (loggingEnabled && logKeyEventsEnabled) {
-            logger.logKeyEvent(String.format(LOCALE, "%sshouldOverrideKeyEvent() 1/2 Event : %s", SPACE, event));
-            logger.logKeyEvent(String.format(LOCALE, "%sshouldOverrideKeyEvent() 1/2 Result: %s", SPACE, retVal));
+            logger.logKeyEvent(String.format(LOCALE, "%s shouldOverrideKeyEvent() 1/2 EVENT   : %s", SPACE, event));
+            logger.logKeyEvent(String.format(LOCALE, "%s shouldOverrideKeyEvent() 2/2 OVERRIDE: %s", SPACE, retVal));
         }
     }
 
     public void onUnhandledKeyEvent(final WebView view, final KeyEvent event) {
         if (loggingEnabled && logKeyEventsEnabled) {
-            logger.logKeyEvent(String.format(LOCALE, "%sonUnhandledKeyEvent() %s", SPACE, event));
+            logger.logKeyEvent(String.format(LOCALE, "%s onUnhandledKeyEvent() %s", SPACE, event));
         }
     }
 
     public void onScaleChanged(final WebView view, final float oldScale, final float newScale) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonScaleChanged() old: %s, new: %s", SPACE, oldScale, newScale));
+            logger.log(String.format(LOCALE, "%s onScaleChanged() old: %s, new: %s", SPACE, oldScale, newScale));
         }
     }
 
     public void onReceivedLoginRequest(final WebView view, final String realm, final String account, final String args) {
         if (loggingEnabled) {
-            logger.logSecurity(String.format(LOCALE, "%sonReceivedLoginRequest() %s, %s, %s", SPACE, realm, account, args));
+            logger.logSecurity(String.format(LOCALE, "%s onReceivedLoginRequest() %s, %s, %s", SPACE, realm, account, args));
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void onRenderProcessGone(final WebView view, final RenderProcessGoneDetail detail, boolean retVal) {
         if (loggingEnabled) {
-            logger.log(String.format(LOCALE, "%sonRenderProcessGone(): %s", SPACE, retVal));
+            logger.log(String.format(LOCALE, "%s onRenderProcessGone() 1/2 DETAIL: %s", SPACE, detail));
+            logger.log(String.format(LOCALE, "%s onRenderProcessGone() 2/2 RESULT: %s", SPACE, retVal));
         }
     }
 
--- library/src/main/java/uk/co/alt236/webviewdebug/StringUtils.java
+++ library/src/main/java/uk/co/alt236/webviewdebug/StringUtils.java
@@ -3,8 +3,6 @@ package uk.co.alt236.webviewdebug;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.webkit.ClientCertRequest;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 
 import java.util.Arrays;
@@ -24,29 +22,11 @@ import java.util.Arrays;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public static String toString(final WebResourceRequest request) {
-        if (request == null) {
-            return "<NULL>";
-        } else {
-            return request.getMethod() + " " + request.getUrl() + " " + request.getRequestHeaders();
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public static String toString(final WebResourceResponse response) {
         if (response == null) {
             return "<NULL>";
         } else {
             return response.getStatusCode() + " " + response.getReasonPhrase();
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public static String toString(final WebResourceError error) {
-        if (error == null) {
-            return "<NULL>";
-        } else {
-            return error.getErrorCode() + " " + error.getDescription();
         }
     }
 }
